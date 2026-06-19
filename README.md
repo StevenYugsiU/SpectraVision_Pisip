5b3b4e71bd44c74c50130b976aa7eaca39d16a4d
Steven Yugsi <syugsi@point.com.ec>
2026-06-19 09:52:08 -0500

    Primer commit del proyecto Pisip

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpectraVision_Pisip
---
+++ src/main/java/com/uisrael/spectraVisionPisip/SpectraVisionPisipApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.spectraVisionPisip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectraVisionPisipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectraVisionPisipApplication.class, args);
+	}
+
+}
